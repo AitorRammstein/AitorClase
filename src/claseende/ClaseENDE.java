c8a8cf17f4cdc093cb6fa9434b4e1ddacd6c1213
AitorRammstein <aitor_flash@hotmail.com>
2017-03-13 08:44:16 +0100

    Inicio del proyecto

--- src/claseende/ClaseENDE.java
+++ src/claseende/ClaseENDE.java
@@ -4,5 +4,6 @@ public class ClaseENDE {
 
     public static void main(String[] args) {
        System.out.println("Inicio del proyecto");
+       System.out.println("Inicio del proyecto2");
     } 
 }
